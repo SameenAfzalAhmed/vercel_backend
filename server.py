bf1a26f9b2ca11cd0248326c1bd618c9ed68c6e9
Sameen Afzal <sameenafzalahmed@gmail.com>
2026-01-20 11:44:21 -0500

    Make env vars safe + add Dockerfile for Fly.io

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.getenv['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ['DB_NAME']]
 
